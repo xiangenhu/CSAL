3101b8d4b0ddd639d21434e3ff37f585bfeb316e
Liang Zhang <lzhang13@memphis.edu>
2020-11-05 04:52:24 -0600

    Update

---
+++ sim_01/simStudents.js
@@ -0,0 +1,223 @@
+var simStudents={
+		Lesson0:
+		{
+		  "lessonId": "lesson0",
+		  "pages": "[]"
+		},
+	Lesson1:
+		{
+		  "lessonId": "lesson1",
+		  "pages": "[]"
+		},
+	Lesson2:
+		{
+		  "lessonId": "lesson2",
+		  "pages": "[]"
+		},
+	Lesson3:
+		{
+		  "lessonId": "lesson3",
+		  "pages": "[]"
+		},
+	Lesson4:
+	{
+		"lessonId": "lesson4",
+		"pages":[
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Diagnostic_1.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Diagnostic_2.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Diagnostic_3.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Diagonstic_4.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Diagonstic_5.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Easy_1.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Easy_1_.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Easy_2.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Easy_3.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Easy_4.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Easy_5.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Easy_6.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Easy_7.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Easy_8.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Easy_9.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Easy_10.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Hard_1.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Hard_2.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Hard_3.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Hard_4.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Hard_5.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Hard_6.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Hard_7.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Hard_8.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Hard_9.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level1_Hard_10.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level3_1.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level3_2.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level3_3.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level3_4.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level3_5.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level3_6.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level3_7.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level3_8.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level3_9.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level3_10.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level3_11.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level3_12.html",
+			 Answer:2
+			},
+			{
+			 Type:"QuestionPage",
+			 MediaPath: "Lesson4_Level3_Open.html",
+			 Answer:2
+			},
+		]
+	}
+}
